e53b9cb1c4e6f3ea7de2391ada35e761a9ef120a
UniKroLL <s.chagin@yandex.ru>
2018-04-10 09:49:53 +0500

    first chapter end

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     paper.install(window);
     paper.setup(document.getElementById('mainCanvas'));
     //TO DO
-    var c;
+    /*var c;
     for(var x=25; x<400; x+=50){
         console.log(x);
         for(var y=25; y<400; y+=50){
@@ -12,7 +12,20 @@ $(document).ready(function(){
             c = Shape.Circle(x, y, 20);
             c.fillColor = 'green';        
         }
-    }
+    }*/
+var c = Shape.Circle(200, 200, 80);
+c.fillColor = 'black';
+var text = new PointText(200, 200);
+text.justification = 'center';
+text.fillColor = 'white';
+text.fontSize = 20;
+text.content = 'hello world';
+
+var tool = new Tool();
+tool.onMouseDown = function(event){
+    var c = Shape.Circle(event.point.x, event.point.y, 20);
+    c.fillColor = 'green';
+}
     paper.view.draw();
     console.log('main.js loaded');
 });
