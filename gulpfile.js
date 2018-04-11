e03034c4dca5965c86deb8a5d77aac79f958322e
UniKroLL <s.chagin@yandex.ru>
2018-04-11 15:48:44 +0500

    second chapter end

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const eslint = require('gulp-eslint');
+
+gulp.task('default', function(){
+    // Запуск ESLint
+    gulp.src(["es6/**/*.js", "public/es6/**/*.js"])
+        .pipe(eslint())
+        .pipe(eslint.format());
+    // Исходный код для Node
+    gulp.src("es6/**/*.js")
+        .pipe(babel())
+        .pipe(gulp.dest("dist"));
+    // Исходный код для браузера
+    gulp.src("public/es6/**/*.js")
+        .pipe(babel())
+        .pipe(gulp.dest("public/dist"));
+    
+});
--- main.js
+++ main.js
@@ -13,19 +13,19 @@ $(document).ready(function(){
             c.fillColor = 'green';        
         }
     }*/
-var c = Shape.Circle(200, 200, 80);
-c.fillColor = 'black';
-var text = new PointText(200, 200);
-text.justification = 'center';
-text.fillColor = 'white';
-text.fontSize = 20;
-text.content = 'hello world';
+    var c = Shape.Circle(200, 200, 80);
+    c.fillColor = 'black';
+    var text = new PointText(200, 200);
+    text.justification = 'center';
+    text.fillColor = 'white';
+    text.fontSize = 20;
+    text.content = 'hello world';
 
-var tool = new Tool();
-tool.onMouseDown = function(event){
-    var c = Shape.Circle(event.point.x, event.point.y, 20);
-    c.fillColor = 'green';
-}
+    var tool = new Tool();
+    tool.onMouseDown = function(event){
+        var c = Shape.Circle(event.point.x, event.point.y, 20);
+        c.fillColor = 'green';
+    };
     paper.view.draw();
     console.log('main.js loaded');
 });
